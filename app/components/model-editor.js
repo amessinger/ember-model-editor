07a23f7b98a4172ebea7c1b86152c0f389260457
Antonin Messinger <antonin.messinger@gmail.com>
2018-08-14 13:56:06 +0200

    refactor a bit

--- app/components/model-editor.js
+++ app/components/model-editor.js
@@ -4,23 +4,30 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   store: service(),
-  value: computed('model', function() {
-    return JSON.stringify(this.model.serialize(), null, 2);
+  includeId: false,
+  value: computed('model', 'includeId', function() {
+    return JSON.stringify(this.model.serialize({ includeId: this.includeId }), null, 2);
   }),
+  updateModel(payload) {
+    let { modelName } = this.model.constructor;
+    let serializer = this.store.serializerFor(modelName);
+
+    // When serializing a record (that's is not embedded), it's 'id' is excluded
+    // from the result by default (depending on includeId=false).
+    // In this case we add it ourselves:
+    if (!payload.id) {
+      payload.id = this.model.id;
+    }
+
+    let resourceHash = serializer.normalize(this.store.modelFor(modelName), payload);
+    this.store.push(resourceHash);
+  },
   actions: {
     doUpdate() {
-      let { modelName } = this.model.constructor;
-      let serializer = this.store.serializerFor(modelName);
-
       let json = this.$('textarea').val();
       let payload = JSON.parse(json);
 
-      // id is not accessible to the user (and not included *by default* in serialize)
-      // so we add it programmatically
-      payload.id = this.model.id;
-
-      let resourceHash = serializer.normalize(this.store.modelFor(modelName), payload);
-      this.store.push(resourceHash);
+      this.updateModel(payload);
     }
   }
 });
